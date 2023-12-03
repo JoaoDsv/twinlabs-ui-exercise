2766e2981de05ce04a4a92b6b52480b6b5ae43d7
Joao Da Silva Marly <joao.pro.dsvm@gmail.com>
2023-12-03 14:07:08 +0100

    Add basic Home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,24 @@
+import "./Home.css";
+import { WorkflowCard } from "../components/WorkflowCard";
+
 export function Home() {
   return (
-    <>
-      <h1>Home page</h1>
-      <p>Now, build your components and routes</p>
-    </>
+    <div className="home-page-wrapper">
+      <div className="home-page-body">
+        <h1 className="home-page-body-title title-1">Welcome, Hugo 👋</h1>
+        <p className="title-2">
+          Select a workflow you want to automate, then answer a few questions to
+          customise it, and let it run in the background for you.
+        </p>
+        <div className="home-page-section-wrapper">
+          <WorkflowCard />
+          <WorkflowCard />
+          <WorkflowCard />
+          <WorkflowCard />
+          <WorkflowCard />
+          <WorkflowCard />
+        </div>
+      </div>
+    </div>
   );
 }
