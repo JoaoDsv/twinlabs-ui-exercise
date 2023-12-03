caec6a234be6c81e389c9b5139362c5bf25701af
Joao Da Silva Marly <joao.pro.dsvm@gmail.com>
2023-12-03 12:39:33 +0100

    Add basic <IntegrationCircle> component

---
+++ src/components/IntegrationCircle.jsx
@@ -0,0 +1,11 @@
+import "./IntegrationCircle.css";
+
+export function IntegrationCircle() {
+  return (
+    <img
+      className="integration-circle"
+      src="/integration-gmail.svg"
+      alt="This workflow includes Gmail integration"
+    />
+  );
+}
