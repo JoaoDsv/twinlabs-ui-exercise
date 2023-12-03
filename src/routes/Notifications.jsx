4bc13f41ee5e43bd948b28be598e1033f969bf69
Joao Da Silva Marly <joao.pro.dsvm@gmail.com>
2023-12-03 19:09:26 +0100

    Add basic Notifications page

--- src/routes/Notifications.jsx
+++ src/routes/Notifications.jsx
@@ -1,7 +1,26 @@
+import "./Notifications.css";
+import { SettingsButton } from "../components/SettingsButton";
+import { LogOutButton } from "../components/LogOutButton";
+import { NotificationItem } from "../components/NotificationItem";
+
 export function Notifications() {
   return (
     <>
-      <h1>Notifications page</h1>
+      <div className="notifications-page-header">
+        <h1 className="title-1">Notifications page</h1>
+        <div className="notifications-page-header-right">
+          <SettingsButton />
+          <LogOutButton />
+        </div>
+      </div>
+      <div className="notifications-page-body">
+        <NotificationItem />
+        <NotificationItem />
+        <NotificationItem />
+        <NotificationItem />
+        <NotificationItem />
+        <NotificationItem />
+      </div>
     </>
   );
 }
