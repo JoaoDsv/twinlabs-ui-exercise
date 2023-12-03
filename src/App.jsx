0100e545b8f550b21242d8812dd36ba0120a12f8
Joao Da Silva Marly <joao.pro.dsvm@gmail.com>
2023-12-03 10:11:18 +0100

    Add "react-router-dom" and 2 empty routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Link,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import { Home } from "./routes/Home";
+import { Notifications } from "./routes/Notifications";
+
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/notifications" element={<Notifications />} />
+      </Route>
+    )
+  );
+
   return (
     <div className="app-wrapper">
       <RouterProvider router={router} />
@@ -6,5 +27,19 @@ function App() {
   );
 }
 
+function RootLayout() {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/notifications">Notifications</Link>
+      </nav>
+
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
 
 export default App;
---
+++ src/routes/Home.jsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <p>Now, build your components and routes</p>
+    </>
+  );
+}
