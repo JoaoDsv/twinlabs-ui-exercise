3b12b63e73b3314edfe9b77f217f1a25bdc06f34
Joao Da Silva Marly <joao.pro.dsvm@gmail.com>
2023-12-03 17:32:13 +0100

    Add "Set a new workflow" button to SideBar

--- src/components/navigation/SideBar.jsx
+++ src/components/navigation/SideBar.jsx
@@ -6,49 +6,57 @@ import "./SideBar.css";
 export function SideBar() {
   return (
     <nav className="sidebar-wrapper">
-      <div className="sidebar-header">
-        <img
-          className="sidebar-header-logo"
-          src="/logo-twin.svg"
-          alt="Twin's logo"
-        />
-        <Link to="/notifications" className="sidebar-header-notifications">
+      <div className="sidebar-top">
+        <div className="sidebar-header">
           <img
-            src="/icon-bell.svg"
-            alt="Bell icon, link to notifications page"
+            className="sidebar-header-logo"
+            src="/logo-twin.svg"
+            alt="Twin's logo"
           />
-        </Link>
-      </div>
-      <div>
-        <h3 className="sidebar-section-title title-3">Active</h3>
-        <div className="sidebar-workflow">
-          <Link to="/" className="sidebar-workflow-link title-3 color-white">
-            <span className="sidebar-workflow-link--light">News tracker |</span>
-            Gekko
-            <div className="sidebar-workflow-integrations">
-              <IntegrationCircle />
-              <IntegrationCircle />
-              <IntegrationCircle />
+          <Link to="/notifications" className="sidebar-header-notifications">
+            <img
+              src="/icon-bell.svg"
+              alt="Bell icon, link to notifications page"
+            />
+          </Link>
+        </div>
+        <div>
+          <h3 className="sidebar-section-title title-3">Active</h3>
+          <div className="sidebar-workflow">
+            <Link to="/" className="sidebar-workflow-link title-3 color-white">
+              <span className="sidebar-workflow-link--light">
+                News tracker |
+              </span>
+              Gekko
+              <div className="sidebar-workflow-integrations">
+                <IntegrationCircle />
+                <IntegrationCircle />
+                <IntegrationCircle />
+              </div>
+            </Link>
+            <div className="sidebar-workflow-time tiny-1 color-white">
+              12h ago
             </div>
-          </Link>
-          <div className="sidebar-workflow-time tiny-1 color-white">
-            12h ago
+          </div>
+        </div>
+        <div>
+          <h3 className="sidebar-section-title title-3">Past</h3>
+          <div className="sidebar-workflow">
+            <Link to="/" className="sidebar-workflow-link title-3 color-white">
+              <span className="sidebar-workflow-link--light">
+                Company research |
+              </span>
+              OpenAI
+            </Link>
+            <div className="sidebar-workflow-time tiny-1 color-white">
+              12h ago
+            </div>
           </div>
         </div>
       </div>
-      <div>
-        <h3 className="sidebar-section-title title-3">Past</h3>
-        <div className="sidebar-workflow">
-          <Link to="/" className="sidebar-workflow-link title-3 color-white">
-            <span className="sidebar-workflow-link--light">
-              Company research |
-            </span>
-            OpenAI
-          </Link>
-          <div className="sidebar-workflow-time tiny-1 color-white">
-            12h ago
-          </div>
-        </div>
+      <div className="sidebar-footer">
+        <button className="button-dark">Set a new workflow</button>
+        <p className="sidebar-footer-version">Alpha v0.1.0</p>
       </div>
     </nav>
   );
